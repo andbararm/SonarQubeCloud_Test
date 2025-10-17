2a3669024619334617d4c0c6ed9bce16ad815c34
Andreas Barth <87762349+andbararm@users.noreply.github.com>
2025-10-17 17:44:18 +0200

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -118,30 +118,29 @@ int main (void) {
   osKernelStart();                      // Start thread execution
 
   //for (;;) {}
-LOOP2:
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  __NOP();
-  goto LOOP2;
+LOOP:
+  printf("0\n");
+  printf("1\n");
+  printf("2\n");
+  printf("3\n");
+  printf("4\n");
+  printf("5\n");
+  printf("6\n");
+  printf("7\n");
+  printf("8\n");
+  printf("9\n");
+  printf("10\n");
+  printf("11\n");
+  printf("12\n");
+  printf("13\n");
+  printf("14\n");
+  printf("15\n");
+  printf("16\n");
+  printf("17\n");
+  printf("18\n");
+  printf("19\n");
+  printf("20\n");
+  goto LOOP;
 }
 
 
